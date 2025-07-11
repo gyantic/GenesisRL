16d779f1a8ac1f6db3d02bd5151319bdc12d3eca
gyantic <kkimura330mp@gmail.com>
2025-07-12 00:00:07 +0900

    FIX: Convert sub-repositories to regular directories

---
+++ Genesis/examples/locomotion/go2_eval.py
@@ -0,0 +1,72 @@
+import argparse
+import os
+import pickle
+from importlib import metadata
+
+import torch
+
+"""
+try:
+    try:
+        if metadata.version("rsl-rl"):
+            raise ImportError
+    except metadata.PackageNotFoundError:
+        if metadata.version("rsl-rl-lib") != "2.2.4":
+            raise ImportError
+except (metadata.PackageNotFoundError, ImportError) as e:
+    raise ImportError("Please uninstall 'rsl_rl' and install 'rsl-rl-lib==2.2.4'.") from e
+"""
+from rsl_rl.runners import OnPolicyRunner
+
+import genesis as gs
+
+from go2_env import Go2Env
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
+    parser.add_argument("--ckpt", type=int, default=100)
+    args = parser.parse_args()
+
+    gs.init(backend=gs.cpu)
+
+    log_dir = f"logs/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"logs/{args.exp_name}/cfgs.pkl", "rb"))
+    reward_cfg["reward_scales"] = {}
+
+    env = Go2Env(
+        num_envs=1,
+        env_cfg=env_cfg,
+        obs_cfg=obs_cfg,
+        reward_cfg=reward_cfg,
+        command_cfg=command_cfg,
+        show_viewer=False,
+    )
+
+    runner = OnPolicyRunner(env, train_cfg, log_dir)
+    resume_path = os.path.join(log_dir, f"model_{args.ckpt}.pt")
+    runner.load(resume_path)
+    policy = runner.get_inference_policy()
+    env.cam.start_recording()
+
+    obs, _ = env.reset()
+    max_steps = 1000
+    step_count = 0
+    with torch.no_grad():
+        while step_count < max_steps:
+            actions = policy(obs)
+            obs, rews, dones, infos = env.step(actions)
+            step_count += 1
+            env.cam.render()
+
+    env.cam.stop_recording(save_to_filename="video_eval.mp4", fps=30)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+# evaluation
+python examples/locomotion/go2_eval.py -e go2-walking -v --ckpt 100
+"""
